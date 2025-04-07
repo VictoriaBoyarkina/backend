9d169700f24bbccebad6caa6217eca12a5b5f0bf
Виктория Бояркина <viktoriaboarkina@MacBook-Air-uzivatela-Viktoria.local>
2025-04-07 17:52:15 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+
+  console.log('App is served on port: ', process.env.PORT);
+}
+
+void bootstrap();
